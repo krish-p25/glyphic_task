18d860be2f236e15f38d35366e7a3a1b14858a49
Krish P <80683788+krish-p25@users.noreply.github.com>
2026-02-23 17:05:03 +0000

    added claude model initiation

---
+++ backend/src/models/claude.js
@@ -0,0 +1,12 @@
+const { Anthropic } = require('@anthropic-ai/sdk')
+const path = require('path');
+require('dotenv').config({
+    path: path.resolve(__dirname, '../../../.env'),
+    quiet: true
+});
+
+const anthropic = new Anthropic({
+    apiKey: process.env.ANTHROPIC_API_KEY
+})
+
+module.exports = anthropic
