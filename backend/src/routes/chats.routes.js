febb5065d8b1807499428c0ef5ffb48e39fb89f4
Krish P <80683788+krish-p25@users.noreply.github.com>
2026-02-23 15:52:32 +0000

    added route for getting all cahts

--- backend/src/routes/chats.routes.js
+++ backend/src/routes/chats.routes.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const chatsController = require('../controllers/chats.controller.js');
 
+//GET /api/chat/get-chats
+// Gets most recent chats, limit 50
+router.get(
+    '/get-chats',
+    chatsController.getChats
+)
+
 // POST /api/chat/create
 // Creates a new chat session
 router.post(
