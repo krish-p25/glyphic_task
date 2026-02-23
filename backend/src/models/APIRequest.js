889c94e41b1839ebce2b7ce1a4b99ab49b311484
Krish P <80683788+krish-p25@users.noreply.github.com>
2026-02-23 17:04:39 +0000

    added APIRequest data object

---
+++ backend/src/models/APIRequest.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./config.js');
+
+const APIRequest = sequelize.define('APIRequest', {
+    uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        unique: true,
+        primaryKey: true
+    },
+    chat_id: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    api_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    timestamp: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+    }
+}, {
+    timestamps: false,
+    tableName: 'api_requests'
+});
+
+module.exports = {
+    APIRequest
+};
