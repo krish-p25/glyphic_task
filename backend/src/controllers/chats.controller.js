5859e6ef60feb7ce661e38cb7007238eda2d820e
Krish P <80683788+krish-p25@users.noreply.github.com>
2026-02-23 17:24:44 +0000

    /create endpoint finds relevant glyhic api endpoint

--- backend/src/controllers/chats.controller.js
+++ backend/src/controllers/chats.controller.js
@@ -1,5 +1,7 @@
 const { Chat } = require('../models/Chat.js');
-const axios = require('axios');
+const { APIRequest } = require('../models/APIRequest.js');
+
+const { anthropic } = require('../models/claude.js');
 
 //Get existing chats
 async function getChats(req, res) {
@@ -18,9 +20,56 @@ async function getChats(req, res) {
 // Create a new chat session
 async function createChat(req, res) {
     try {
-        const { title, description } = req.body;
-        const chat = await Chat.create({ title, description });
-        res.status(201).json(chat);
+        const { message } = req.body;
+        const newChat = await Chat.create({
+            title: message,
+            timestamp: Date.now()
+        });
+
+        const claudeResponse = await anthropic.messages.create({
+            model: "claude-haiku-4-5",
+            max_tokens: 500,
+            system: [
+                {
+                    type: 'text',
+                    text: `You are a data analyst assistant`
+                }
+            ],
+            messages: [
+                {
+                    role: 'user',
+                    content: [
+                        {
+                            type: 'text',
+                            text:
+                            `This is the query from your user: ${message}` + 
+                            'Your first step is to build the API request needed to get the relevant information' + 
+                            'The parameters in the API request are: ' +
+                            'participant_email (string)' + 
+                            'start_time_from (UTC ISO 8601 string)' + 
+                            'start_time_to (UTC ISO 8601 string)' + 
+                            'title_filter (string)' + 
+                            'cursor (string)' + 
+                            'limit (integer max 100)' +
+                            'the base url for this api call is https://api.staging.glyphic.ai/v1/calls?' + 
+                            'add the relevant parameters to the end of this URL and respond with just the API request URL' + 
+                            'do not include any other text in your response other than just the API URL'
+                            
+                        }
+                    ]
+                }
+            ]
+        })
+
+        const parsedAPIURL = claudeResponse.content[0].text
+
+        const newAPIRequest = await APIRequest.create({
+            chat_id: newChat.uuid,
+            api_url: parsedAPIURL,
+            timestamp: Date.now()
+        })
+
+        res.status(201).json(newChat);
     }
     catch (error) {
         console.error('❌ Error creating chat:', error);
