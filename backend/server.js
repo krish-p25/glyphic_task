9764de20f7026c2633fd3a2fe59c82b6dd83b259
Krish P <80683788+krish-p25@users.noreply.github.com>
2026-02-23 14:17:59 +0000

    changed port to 3001

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config({ quiet: true });
 const GLYPHIC_API_KEY = process.env.GLYPHIC_API_KEY;
 const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
