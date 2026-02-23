a2232ae704e5dc463bd667015c39348d2a2f9ee4
Krish P <80683788+krish-p25@users.noreply.github.com>
2026-02-23 15:09:20 +0000

    added database connection test when server starts

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const app = express();
+const app = require('./src/app');
 require('dotenv').config({ quiet: true });
 const GLYPHIC_API_KEY = process.env.GLYPHIC_API_KEY;
 const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
 const PORT = process.env.PORT || 3001;
+
+//Test the database connection
+const { testConnection } = require('./src/models/config.js');
+testConnection();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
